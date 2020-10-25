9522fc196e69579acf8555d2327bfcc12180a5c1
Xavier Gastaud <xgastaud@gmail.com>
2020-10-25 20:46:02 +0100

    added authentication via devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home', to: 'pages#home'
   get 'ask', to: 'pages#ask'
   get 'answer', to: 'pages#answer'
+  root to: "pages#home"
 end
